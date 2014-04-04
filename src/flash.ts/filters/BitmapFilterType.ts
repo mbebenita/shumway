bc3f353d16cb6324ffaa98b945ec0fe71baca162
Claus Wahlers <claus@codeazur.com.br>
2014-04-04 20:29:58 -0300

    flash.filters native bindings

--- src/flash.ts/filters/BitmapFilterType.ts
+++ src/flash.ts/filters/BitmapFilterType.ts
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013 Mozilla Foundation
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,33 +15,31 @@
  */
 // Class: BitmapFilterType
 module Shumway.AVM2.AS.flash.filters {
-  import notImplemented = Shumway.Debug.notImplemented;
+
   export class BitmapFilterType extends ASNative {
-    
+
     // Called whenever the class is initialized.
     static classInitializer: any = null;
-    
+
     // Called whenever an instance of the class is initialized.
     static initializer: any = null;
-    
+
     // List of static symbols to link.
     static staticBindings: string [] = null; // [];
-    
+
     // List of instance symbols to link.
     static bindings: string [] = null; // [];
-    
+
     constructor () {
       false && super();
-      notImplemented("Dummy Constructor: public flash.filters.BitmapFilterType");
     }
-    
+
     // JS -> AS Bindings
     static INNER: string = "inner";
     static OUTER: string = "outer";
     static FULL: string = "full";
-    
-    
+
     // AS -> JS Bindings
-    
+
   }
 }
